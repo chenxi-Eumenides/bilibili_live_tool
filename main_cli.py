60db2261b8ba68d5c81c58cfae7d35fdad8c9efc
chenxi-Eumenides <1742041477@qq.com>
2025-07-15 22:00:25 +0800

    推流码的显示逻辑放进bili.py中

---
+++ main_cli.py
@@ -0,0 +1,74 @@
+from sys import argv
+
+from src.bili import Bili_Live
+from src.utils import is_exist, log
+
+
+def help(live: Bili_Live):
+    log(live.get_help_info())
+
+
+def title(live: Bili_Live):
+    live.set_live_title()
+    pass
+
+
+def area(live: Bili_Live):
+    live.set_live_area()
+    pass
+
+
+def manual(live: Bili_Live):
+    status = live.get_live_status()
+    if status == 0 or status == 2:
+        live.set_live_area()
+        print("")
+        live.set_live_title()
+        print("")
+        live.start_live()
+        print("")
+        live.get_rtmp()
+    elif status == 1:
+        live.stop_live()
+    else:
+        return False
+
+
+def auto(live: Bili_Live):
+    status = live.get_live_status()
+    if status == 0 or status == 2:
+        live.start_live()
+        print("")
+        live.get_rtmp()
+    elif status == 1:
+        live.stop_live()
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    live = Bili_Live(config_file="config.json")
+    live.login()
+
+    log("")
+    log(live.get_room_info())
+    log("")
+
+    option = argv[1] if len(argv) > 1 else None
+    if option == "auto":
+        auto(live)
+    elif option == "manual":
+        manual(live)
+    elif option == "title":
+        title(live)
+    elif option == "area":
+        area(live)
+    elif option == "help":
+        help(live)
+    elif option == "info":
+        pass
+    else:
+        if is_exist(live._config_.config_file):
+            auto(live)
+        else:
+            manual(live)
