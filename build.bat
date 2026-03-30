aa901c0cbf2199a5fecf111400d6db06d0bacfdd
chenxi-Eumenides <1742041477@qq.com>
2026-03-31 03:19:38 +0800

    更新文件、说明

--- build.bat
+++ build.bat
@@ -1,2 +1,4 @@
 uv sync
+
 uv run --dev pyinstaller -n "bili-live-tool" -c -i .\src\static\bili-icon.ico --onefile --clean .\cli\main.py
+uv run --dev pyinstaller -n "bili-live-tool-tui" -c -i .\src\static\bili-icon.ico --onefile --clean tui/app/main.py
