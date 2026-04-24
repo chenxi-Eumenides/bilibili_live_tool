6e44509e8d90d59faf80aafe0bc6e6377aee9dd4
chenxi-Eumenides <1742041477@qq.com>
2026-04-24 23:07:03 +0800

    修复构建脚本适配新的分支结构

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
 @echo off
 uv sync
 
-printf "\n========== packaging TUI version ==========\n\n"
+printf "\n========== packaging exe ==========\n\n"
 uv run --group build pyinstaller ^
-    -n "bili-live-tool-tui" ^
+    -n "bili-live-tool" ^
     -c -i .\src\static\bili-icon.ico ^
     --collect-submodules src ^
     --add-data=".\src\ui\styles;.\src\ui\styles" ^
